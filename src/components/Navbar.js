7098af3bf9d9309e4aac646a375d979a86a025db
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2022-01-24 12:58:25 +0530

    Add navbar component with relevant styles

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import '../styles/Navbar.css';
+
+import shoppingCartIcon from '../icons/cart-shopping-solid.svg';
+
+function Navbar(props) {
+  return (
+    <header>
+      <nav id="navbar">
+        <h1>{props.brandName}</h1>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/products">Products</a>
+          </li>
+          <li>
+            <a href="/cart">
+              <img src={shoppingCartIcon} height={18} alt="Shopping cart"></img>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
