1e2651f22bd3d4300a166b94f62384fe61517e92
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2022-01-30 18:55:02 +0530

    Implement routes for the pages

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import './App.css';
 import Products from './components/Products';
 import Navbar from './components/Navbar';
 import Cart from './components/Cart';
+import Home from './components/Home';
+
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Cart />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
